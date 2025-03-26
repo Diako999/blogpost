da09bf99b7c2d73ccfbd09649aef6e0398463f74
Diako <diakoilbagy@gmail.com>
2025-03-26 05:35:46 -0400

    create post api

--- blogs/urls.py
+++ blogs/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from .views import blogs, createPost, BlogListView, BlogListCreateView
+from .views import blogs, createPost ,BlogListView, BlogListView, BlogListCreateView
 urlpatterns = [
     path('', blogs, name='blogs'),
     path('create/', createPost, name='create'),
     path('api/', BlogListView.as_view(), name='api'),
+    path('api/create', BlogListCreateView.as_view(), name='post'),
 ]
--- blogs/views.py
+++ blogs/views.py
@@ -2,46 +2,42 @@ from django.shortcuts import render, redirect
 from rest_framework.response import Response
 from rest_framework.generics import ListCreateAPIView
 from rest_framework.permissions import IsAuthenticated
-from django.contrib.auth.decorators import login_required
 from rest_framework.views import APIView
+from django.contrib.auth.decorators import login_required
 from .models import Blog
 from .serializers import BlogSerializer
 from .forms import CreatePost
 
+# ✅ Protect API: Only authenticated users can access
+class BlogListCreateView(ListCreateAPIView):
+    queryset = Blog.objects.all()
+    serializer_class = BlogSerializer
+    permission_classes = [IsAuthenticated]  
+
 class BlogListView(APIView):
-    permission_classes = [IsAuthenticated] 
+    permission_classes = [IsAuthenticated]  
+
     def get(self, request):
         blogs = Blog.objects.all()
         serializer = BlogSerializer(blogs, many=True)
         return Response(serializer.data)
-    
-class BlogListCreateView(ListCreateAPIView):
-    queryset = Blog.objects.all()
-    serializer_class = BlogSerializer
-    permission_classes = [IsAuthenticated]
+
+# ✅ Protect Template Views
+@login_required
 def blogs(request):
-    blogs = Blog.objects.all().order_by('-created_at') 
-    context = {
-        'blogs': blogs
-    }
-    return render(request, 'blogs/blogs.html', context)
+    blogs = Blog.objects.all().order_by('-created_at')
+    return render(request, 'blogs/blogs.html', {'blogs': blogs})
+
 @login_required
 def createPost(request):
     if request.method == 'POST':
         form = CreatePost(request.POST)
-        if form.is_valid():  # Add parentheses
+        if form.is_valid():
             blog = form.save(commit=False)
             blog.author = request.user
             blog.save()
             return redirect('home')
-        else:
-            print("Form is invalid")  # Debug line
-            print(form.errors)  # Print any form errors
     else:
-        form = CreatePost()  # Correct form initialization
+        form = CreatePost()
 
-    context = {
-        'form': form
-    }
-
-    return render(request, 'blogs/createBlog.html', context)
+    return render(request, 'blogs/createBlog.html', {'form': form})
